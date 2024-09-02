43c3147b60e2dc192e824dbeb848de1d7af76b49
jeongbaaaan <160698547+jeongbaaaan@users.noreply.github.com>
2024-09-02 20:18:12 +0900

    Title: 윤년인가, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240902/윤년인가/is-leap-year.py
@@ -0,0 +1,12 @@
+a = int(input())
+if a % 4 == 0:
+    if y % 100 == 0:
+        if y % 400 == 0:
+            print("true")
+        else:
+             print("false")
+    
+    else:
+        print("true")
+else:
+    print("false")
