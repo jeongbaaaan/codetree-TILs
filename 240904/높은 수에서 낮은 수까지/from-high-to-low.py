2193c9d91854f3f6facdeabfc63142300d140a5f
jeongbaaaan <160698547+jeongbaaaan@users.noreply.github.com>
2024-09-04 17:34:26 +0900

    Title: 높은 수에서 낮은 수까지, Time: 110ms, Memory: 24MB, Status: Passed - CodeTree

--- 240904/높은 수에서 낮은 수까지/from-high-to-low.py
+++ 240904/높은 수에서 낮은 수까지/from-high-to-low.py
@@ -1,7 +1,7 @@
-a, b= map(int, input().split())
+a, b = map(int, input().split())
 if b >= a:
-    for i in range(b, a-1, -1):
-        print(i, end= " ")
+    for i in range(b, a-1 , -1):
+        print(i, end=" ")
 else:
     for i in range(a, b-1, -1):
-        print(i , end=" ")
+        print(i, end=" ")
