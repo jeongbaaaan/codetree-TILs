8b3c645e605fb23a7454cb7b4df1c6238356b600
jeongbaaaan <160698547+jeongbaaaan@users.noreply.github.com>
2024-09-04 15:11:33 +0900

    Title: 짝수만 출력 2, Time: 100ms, Memory: 24MB, Status: Passed - CodeTree

--- 240904/짝수만 출력 2/output-only-even-2.py
+++ 240904/짝수만 출력 2/output-only-even-2.py
@@ -1,4 +1,9 @@
-a, b = map(int, input().split())
-while a <= b:
-    print(a, end=" ")
-    a -= 2
+b, a = map(int, input().split())  # 두 정수 b와 a를 입력받습니다.
+
+# b부터 a까지 거꾸로 탐색하기 위해 현재 값을 b로 초기화합니다.
+current = b
+
+while current >= a:
+    if current % 2 == 0:  # 짝수인지 확인합니다.
+        print(current, end=' ')  # 짝수라면 출력합니다.
+    current -= 2  # 짝수이므로 2씩 감소시킵니다.
