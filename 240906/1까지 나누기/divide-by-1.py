25f92455240e03b954d1d39735a8584ed4958fc6
jeongbaaaan <160698547+jeongbaaaan@users.noreply.github.com>
2024-09-06 16:13:25 +0900

    Title: 1까지 나누기, Time: 100ms, Memory: 24MB, Status: Passed - CodeTree

--- 240906/1까지 나누기/divide-by-1.py
+++ 240906/1까지 나누기/divide-by-1.py
@@ -9,9 +9,10 @@ divisor = 1
 
 # 몫이 1 이하가 될 때까지 나눗셈을 반복합니다.
 while n > 1:
+    n = n // divisor 
     count += 1
     divisor += 1
-    n = n // divisor  # n을 divisor로 나눈 몫으로 업데이트합니다.
+      # n을 divisor로 나눈 몫으로 업데이트합니다.
 
 # 결과를 출력합니다.
 print(count)
