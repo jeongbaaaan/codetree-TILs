24f268bea634ef28c15de46eda044f30bb5096ec
jeongbaaaan <160698547+jeongbaaaan@users.noreply.github.com>
2024-09-08 17:54:34 +0900

    Title: 조건에 따른 연산, Time: 5021ms, Memory: 31MB, Status: Time Limit Exceed - CodeTree

--- 240908/조건에 따른 연산/operatino-by-rule.py
+++ 240908/조건에 따른 연산/operatino-by-rule.py
@@ -2,9 +2,9 @@ n = int(input())
 cnt = 0
 while True:
     if n % 2 == 0:
-        n *3+1
+        n = n*3+1
         cnt += 1
     else:
-        n *2+2
+        n = n*2+2
         cnt += 1
 print(cnt)
