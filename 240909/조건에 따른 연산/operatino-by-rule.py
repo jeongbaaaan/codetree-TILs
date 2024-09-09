e26bd13efd8c5e09fbf08879946a97c9148d8297
jeongbaaaan <160698547+jeongbaaaan@users.noreply.github.com>
2024-09-09 12:02:43 +0900

    Title: 조건에 따른 연산, Time: 152ms, Memory: 24MB, Status: Passed - CodeTree

--- 240909/조건에 따른 연산/operatino-by-rule.py
+++ 240909/조건에 따른 연산/operatino-by-rule.py
@@ -1,15 +1,15 @@
-def find_cnt(n):
-    cnt = 0
-    while n < 1000:
-        if n % 2 == 0:
-            n = n * 3 + 1
-        else:
-            n = n * 2 + 2
-        cnt += 1
-    return cnt
-
-# 입력 받기
 n = int(input())
+cnt = 0
 
-# cnt 값 출력
-print(find_cnt(n))
+while(True):
+    if n >= 1000:
+        break
+    
+    if n % 2 == 0:
+        n = n*3+1
+    else:
+        n = n*2+2
+
+    cnt += 1
+
+print(cnt)
