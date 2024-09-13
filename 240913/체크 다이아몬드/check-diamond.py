78205f22a41be4012b8a8ebbcaead8407162d321
jeongbaaaan <160698547+jeongbaaaan@users.noreply.github.com>
2024-09-13 11:17:21 +0900

    Title: 체크 다이아몬드, Time: 117ms, Memory: 24MB, Status: Passed - CodeTree

--- 240913/체크 다이아몬드/check-diamond.py
+++ 240913/체크 다이아몬드/check-diamond.py
@@ -1,15 +1,22 @@
 def print_check_diamond(n):
     size = 2 * n - 1  # 격자의 크기
-    for i in range(size):
-        # 중앙으로부터의 거리를 구함
-        row = abs(n - i - 1)
-        # 별의 개수는 n-row
-        stars = n - row
-        # 출력할 문자열 생성
-        line = [' '] * size
-        for j in range(stars):
-            # 체크 무늬로 별을 그리기 위해 짝수 열에만 별을 넣음
-            line[n - stars + 2 * j - 1] = '*'
+    
+    # 다이아몬드 윗부분
+    for i in range(n):
+        line = [' '] * size  # 빈 칸으로 채워진 한 줄 생성
+        for j in range(i + 1):
+            line[n - i - 1 + 2 * j] = '*'  # 체크 무늬로 별을 배치
+        print(''.join(line))
+    
+    # 다이아몬드 아랫부분
+    for i in range(n - 1):
+        line = [' '] * size  # 빈 칸으로 채워진 한 줄 생성
+        for j in range(n - i - 1):
+            line[i + 1 + 2 * j] = '*'  # 체크 무늬로 별을 배치
         print(''.join(line))
 
-# n = 3일 때 다이아몬드 출력
+# 입력 값 받기
+n = int(input())
+
+# 다이아몬드 출력
+print_check_diamond(n)
