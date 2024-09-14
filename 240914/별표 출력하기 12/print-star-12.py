5b2ed51a7eff5944731c138deeb1fff36f1de4ce
jeongbaaaan <160698547+jeongbaaaan@users.noreply.github.com>
2024-09-14 21:25:09 +0900

    Title: 별표 출력하기 12, Time: 139ms, Memory: 24MB, Status: Passed - CodeTree

--- 240914/별표 출력하기 12/print-star-12.py
+++ 240914/별표 출력하기 12/print-star-12.py
@@ -1,10 +1,16 @@
-# 정수 n 입력 받기
+# 변수 선언 및 입력
 n = int(input())
 
-# 첫 번째 줄: n개의 별을 출력
-print("* " * n)
-
-# 두 번째 줄부터 마지막 줄까지
-for i in range(1, n):
-    # 공백은 i * 2개, 출력할 별의 개수는 n - i개
-    print(" " * (i * 2) + "* " * (n - i))
+for i in range(n):
+    for j in range(n):
+        if j % 2 == 0:
+            if i == 0:
+                print("* ", end="")
+            else:
+                print("  ", end="")
+        else:
+            if i <= j:
+                print("* ", end="")
+            else:
+                print("  ", end="")
+    print()
