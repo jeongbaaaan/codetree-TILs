5a4bfaab176c7fcce2d4d583987f3df5ffd6dab3
jeongbaaaan <160698547+jeongbaaaan@users.noreply.github.com>
2024-09-16 13:29:19 +0900

    Title: 규칙을 찾아 출력하기, Time: 138ms, Memory: 24MB, Status: Passed - CodeTree

--- 240916/규칙을 찾아 출력하기/find-a-rule-and-print.py
+++ 240916/규칙을 찾아 출력하기/find-a-rule-and-print.py
@@ -3,6 +3,6 @@ n = int(input())
 print("* " * (n - 1) + "*")
 
 for i in range(1, n - 1):
-    print("* " * i + " " * (n - i) + "*")
+    print("* " * i  + "  " * (n - i -1) + "*")
 
 print("* " * (n -1) + "*")
