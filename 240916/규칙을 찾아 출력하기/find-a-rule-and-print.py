9326ab4d6c76c8c1bb7b0f4a69a2001837d697ec
jeongbaaaan <160698547+jeongbaaaan@users.noreply.github.com>
2024-09-16 13:17:36 +0900

    Title: 규칙을 찾아 출력하기, Time: 110ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240916/규칙을 찾아 출력하기/find-a-rule-and-print.py
+++ 240916/규칙을 찾아 출력하기/find-a-rule-and-print.py
@@ -1,11 +1,7 @@
 n = int(input())
 
-for i in range(n):
-    if i % 2 == 0:
-        # 짝수 줄: 별을 n개 출력
-        print('* ' * n)
-    else:
-        # 홀수 줄: 별과 공백 처리
-        stars = '* ' * (i // 2 + 1)
-        spaces = ' ' * (2 * (n - (i // 2 + 1) - 1))
-        print(stars + spaces + '* ' * (i // 2))
+print("* " * n)
+
+
+for i in range(1, n):
+    print("* " * i + " " * (n-i-1) + "*")
