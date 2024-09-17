84bde1205577ebc5b0356db5e7fed8351b40d1ed
jeongbaaaan <160698547+jeongbaaaan@users.noreply.github.com>
2024-09-17 11:40:48 +0900

    Title: 행과 열, Time: 114ms, Memory: 24MB, Status: Passed - CodeTree

--- 240917/행과 열/row-column.py
+++ 240917/행과 열/row-column.py
@@ -1,5 +1,11 @@
-a, b = map(int, input(). split())
-for i in range(1, 5):
-    for j in range(1, 6):
-        print(i * j, end=" ")
+#, b = map(int, input(). split())
+#for i in range(1, 5):
+    #for j in range(1, 6):
+        #print(i * j, end=" ")
+    #print()
+
+a, b = map(int, input().split())
+for i in range(1, a+1):
+    for j in range(1, b+1):
+        print(i*j, end=" ")
     print()
